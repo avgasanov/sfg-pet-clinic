ff683b9ff96cd059bd6c6e119a40e9d87d8100d7
AVGasanov <avgasanov@gmail.com>
2019-03-20 22:55:24 +0400

    Fixed Broken Links. Closes #45

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -21,4 +21,10 @@ public class OwnerController {
 
         return "owners/index";
     }
+
+    @RequestMapping("/owners/find")
+    public String findOwners() {
+
+        return "notimplemented";
+    }
 }
